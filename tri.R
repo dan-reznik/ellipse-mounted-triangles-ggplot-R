5f91961cdccb2559c6a86636309cc4c65fca26ff
Dan Reznik <dreznik@gmail.com>
2020-08-29 11:36:38 -0300

    first commit

---
+++ tri.R
@@ -0,0 +1,79 @@
+law_of_cosines <- function(a,b,c) { (b*b+c*c-a*a)/(2*b*c) }
+
+tri_dist <- function(df_tri,v1,v2) {
+  edist(df_tri[[v1,1]], df_tri[[v1,2]],
+        df_tri[[v2,1]], df_tri[[v2,2]])
+}
+
+tri_homothetic <- function(a,tDeg) {
+  bind_rows(get_ellipse_point(a,tDeg),
+            get_ellipse_point(a,tDeg+120),
+            get_ellipse_point(a,tDeg-120))
+}
+
+tri_sides <- function(df_tri) {
+  s1 <- tri_dist(df_tri,2,3)
+  s2 <- tri_dist(df_tri,3,1)
+  s3 <- tri_dist(df_tri,1,2)
+  c(s1,s2,s3)
+}
+
+tri_cosines <- function(sides) {
+  a <- sides[1]; b <- sides[2]; c <- sides[3];
+  x <- law_of_cosines(a,b,c)
+  y <- law_of_cosines(b,c,a)
+  z <- law_of_cosines(c,a,b)
+  c(x,y,z)
+}
+
+dot3 <- function(v1,v2) {
+  v1[1]*v2[1]+v1[2]*v2[2]+v1[3]*v2[3]
+}
+
+trilin_to_cartesian <- function(df_tri,sides,ts) {
+  #[A, B, C], //(* vertices *)
+  #[a, b, c], // (* side lengths *)
+  #[x, y, z]) //(* trilinears *)
+  v <- c(sides[1]*ts[1],sides[2]*ts[2],sides[3]*ts[3])
+  denom <- v[1]+v[2]+v[3]
+  x <- dot3(v, df_tri$x) / denom
+  y <- dot3(v, df_tri$y) / denom 
+
+  tibble(x=x,y=y)
+}
+
+#function generic_triangle(orbit,sides,ts) {
+#  return ts.map(t=>trilin_to_cartesian(orbit,sides,t));
+#}
+
+tri_generic <- function(df_tri, sides, ts_list) {
+  bind_rows(trilin_to_cartesian(df_tri,sides,ts_list$row1),
+            trilin_to_cartesian(df_tri,sides,ts_list$row2),
+            trilin_to_cartesian(df_tri,sides,ts_list$row3))
+}
+
+tri_orthic <- function(df_tri) {
+  sides <- tri_sides(df_tri)
+  cs <- tri_cosines(sides)
+  secs <- cs %>% map_dbl(~1/.x)
+  ts_list <- list(row1=c(0,secs[2],secs[3]),
+                  row2=c(secs[1],0,secs[3]),
+                  row3=c(secs[1],secs[2],0)) 
+  tri_generic(df_tri,sides,ts_list)
+}
+
+# https://faculty.evansville.edu/ck6/encyclopedia/ETC.html
+# https://github.com/dan-reznik/ellipse-mounted-loci-p5js
+tri_X131 <- function(df_tri) {
+  sides <- tri_sides(df_tri)
+  a <- sides[1]; b <- sides[2]; c <- sides[3]
+  c2 <- c*c; c4 <- c2*c2; c6 <- c2*c4
+  b2 <- b*b; b4 <- b2*b2; b6 <- b2*b4
+  a2 <- a*a; a4 <- a2*a2; a6 <- a2*a4
+  c8 <- c2*c6; b8 <- b2*b6; a8 <- a2*a6
+  v1 <- (a2-b2-c2)*(a4*b2-2*a2*b4+b6+a4*c2+2*a2*b2*c2-b4*c2-2*a2*c4-b2*c4+c6)*(2*a8-3*a6*b2+a4*b4-a2*b6+b8-3*a6*c2+2*a4*b2*c2+a2*b4*c2-4*b6*c2+a4*c4+a2*b2*c4+6*b4*c4-a2*c6-4*b2*c6+c8)
+  v2 <- (-a2+b2-c2)*(a6-2*a4*b2+a2*b4-a4*c2+2*a2*b2*c2+b4*c2-a2*c4-2*b2*c4+c6)*(a8-a6*b2+a4*b4-3*a2*b6+2*b8-4*a6*c2+a4*b2*c2+2*a2*b4*c2-3*b6*c2+6*a4*c4+a2*b2*c4+b4*c4-4*a2*c6-b2*c6+c8)
+  v3 <- (-a2-b2+c2)*(a6-a4*b2-a2*b4+b6-2*a4*c2+2*a2*b2*c2-2*b4*c2+a2*c4+b2*c4)*(a8-4*a6*b2+6*a4*b4-4*a2*b6+b8-a6*c2+a4*b2*c2+a2*b4*c2-b6*c2+a4*c4+2*a2*b2*c4+b4*c4-3*a2*c6-3*b2*c6+2*c8)
+  ts <- c(v1/a,v2/b,v3/c)
+  trilin_to_cartesian(df_tri, sides, ts)
+}
---
+++ utils.R
@@ -0,0 +1,57 @@
+toRad <- function(tDeg) { tDeg*pi/180.0 }
+edist <- function(x1,y1,x2,y2) {
+  dx <- x1-x2
+  dy <- y1-y2
+  sqrt(dx*dx+dy*dy)
+}
+
+
+get_ellipse_point <- function(a,tDeg) {
+  t <- toRad(tDeg)
+  tibble(x=a*sin(t),y=cos(t))
+}
+
+get_ellipse_foci <- function(a) {
+  c = sqrt(a*a-1)
+  tibble(x=c(-c,c),y=c(0,0))
+}
+
+#function orthic_triangle(orbit,sides) {
+#  let [cA,cB,cC]=tri_cosines(sides);
+#  /* can and will overflow */
+#    let secA=1/cA, secB=1/cB, secC=1/cC;
+#    let ts=[[0,secB,secC],
+#            [secA,0,secC],
+#            [secA,secB,0]];
+#    return generic_triangle(orbit,sides,ts);
+#}
+
+get_locus <- function(a,degStep,tri_fn,tri_derived_fn,x_fn) {
+  degs <- seq(0,360,degStep)
+  tris <- degs %>% map(~{tri_fn(a,.x)})
+  tris_derived <- tris %>% map(tri_derived_fn)
+  df_xs <- tris_derived %>% map_dfr(x_fn)
+  df_xs %>% mutate(deg=degs,.before=x)
+}
+
+#tri_homothetic(a,tDeg)
+#tri_orthic(df_tri)
+
+draw_scene <- function(a,tDeg,tri_fn,tri_derived_fn,X_fn,
+                       df_locus) {
+  df_tri <- tri_fn(a,tDeg)
+  df_derived <- tri_derived_fn(df_tri) 
+  df_foci <- get_ellipse_foci(a)
+  df_X <- X_fn(df_derived)
+  ggplot() +
+    geom_ellipse(aes(a = a, b = 1,x0=0,y0=0,angle=0), size=1.0,color="black") +
+    geom_point(aes(x,y),color="black",data=df_foci) +
+    geom_polygon(aes(x,y),fill=NA, size=1.25,color="blue", data=df_tri) + 
+    geom_point(aes(x,y),color="blue",size=2.5, data=df_derived) +
+    geom_polygon(aes(x,y),fill=NA, size=1.25,color="orange", data=df_derived) + 
+    geom_point(aes(x,y),color="orange",size=2.5, data=df_derived) +
+    geom_path(aes(x,y),color="red",size=1.25,data=df_locus) +
+    geom_point(aes(x,y),color="red",size=2.5, data=df_X) +
+    geom_text(aes(x,y),label="X131",color="red",data=df_X,vjust=-.2,hjust=-.2) +
+    coord_fixed()
+}
